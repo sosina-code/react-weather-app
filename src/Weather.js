3c765b2a3df17f8f5de4fb46a1dcedc02fc92e1d
sosina-bug <neth23nean67wwe.com@gmail.com>
2024-09-27 00:38:58 +0300

    html added

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,32 @@
 import React from "react";
+import axios from "axios";
 import"./Weather.css";
 export default function Weather(){
+  return(
+    <div className ="weather">
+      <form>
+        <input type ="search" placeholder="Enter a city.." className="form-control"/>
+        <input type="submit" value= "search" className="btn btn-primary"/>
+      </form>
+      <h1>New York</h1>
+    <ul>
+      <li>Tuesday 20:30</li>
+      <li>mostly cloudy</li>
+    </ul>
+    <div className ="row">
+      <div className ="col-6">
+        <img src ="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="mostly cloudy"/>
+        6°C
+      </div>
+      <div className ="col-6">
+        <ul>
+          <li>precipitation:15%</li>
+          <li>humidity:72%</li>
+          <li>wind:13 km/hr</li>
+        </ul>
+      </div>
+    </div>
+    
+    </div>
+  );
 }
