388eb35d8381cd4a4a812fba5b525448b7b47135
sosina-bug <neth23nean67wwe.com@gmail.com>
2024-09-27 01:09:15 +0300

    added more css

--- src/Weather.js
+++ src/Weather.js
@@ -5,7 +5,7 @@ export default function Weather(){
   return(
     <div className ="weather">
       <form>
-        <input type ="search" placeholder="Enter a city.." className="form-control"/>
+        <input type ="search" placeholder="Enter a city.." className="form-control"  autoFocus="on"/>
         <input type="submit" value= "search" className="btn btn-primary"/>
       </form>
       <h1>New York</h1>
@@ -13,10 +13,15 @@ export default function Weather(){
       <li>Tuesday 20:30</li>
       <li>mostly cloudy</li>
     </ul>
-    <div className ="row">
+    <div className ="row mt-3">
       <div className ="col-6">
-        <img src ="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="mostly cloudy"/>
-        6°C
+        <div className="selector">
+        <img src ="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="mostly cloudy" className="float-left"/>
+        </div>
+        <div className="float-left">
+       <span className="temperature">6</span>
+       <span className="unit"> °C</span>
+       </div>
       </div>
       <div className ="col-6">
         <ul>
@@ -26,7 +31,18 @@ export default function Weather(){
         </ul>
       </div>
     </div>
-    
+    <footer>
+      This project is coded by 
+      <a href="https://www.sosina.com" target="_blank">
+      sosina-code
+      </a>
+      and is 
+      <a href="" target="_blank">
+        open-sourced on github
+      </a>
+    </footer>
     </div>
+    
+    
   );
 }
